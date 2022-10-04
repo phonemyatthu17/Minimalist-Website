31e9a969c8525a60efbb8e157e2c97179062be25
phonemyatthu17 <94057124+phonemyatthu17@users.noreply.github.com>
2022-10-04 10:07:02 +0630

    desktop

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ burger.addEventListener("click", () => {
   if (burger.classList.contains("active")) {
     gsap.to(".links", { x: "100%" });
     gsap.to("line", { stroke: "white" });
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
   } else {
     gsap.to(".links", { x: "0%" });
     gsap.to(".line", { stroke: "black" });
@@ -12,6 +14,25 @@ burger.addEventListener("click", () => {
       { opacity: "0", y: 0 },
       { opacity: "1", y: 20, delay: 0.25, stagger: 0.25 }
     );
+    gsap.set("body", { overflowY: "hidden" });
   }
   burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".videos");
+gsap.set(videos, { opacity: 0 });
+
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top center",
+    end: "bottom center",
+    onEnter: () => {
+      gsap.set(video, { opacity: 1 });
+      video.play();
+    },
+    onEnterBack: () => video.play(),
+    onLeave: () => video.pause(),
+    onLeaveBack: () => video.pause(),
+  });
 });
